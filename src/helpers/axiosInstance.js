ab059e5339b28d2e9377b091321d81751e719e6d
AbhijitDevghare <kishordevghare6@gmail.com>
2025-11-13 16:35:43 +0530

    added the backend url

--- src/helpers/axiosInstance.js
+++ src/helpers/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const BASE_URL= process.env.BACKEND_URL;
+const BASE_URL= "https://techno-gears-backend.vercel.app/api/v1/" || process.env.BACKEND_URL;
 
 const axiosInstance=axios.create()
 
